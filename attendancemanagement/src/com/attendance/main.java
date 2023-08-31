504f6883d8a99a8dc2ecd010ce71c39bdf5af99a
pandatji0903 <118503917+pandatji0903@users.noreply.github.com>
2023-08-31 20:52:53 +0530

    Add files via upload

---
+++ attendancemanagement/src/com/attendance/main.java
@@ -0,0 +1,8 @@
+package com.attendance;
+import java.sql.SQLException;
+public class main {
+    public static void main(String[] args) throws SQLException {
+        Login login = new Login();
+        login.loginView();
+    }
+}
